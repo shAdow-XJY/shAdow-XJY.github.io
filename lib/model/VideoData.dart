67434da7bbe19ee12477e80a3a2e60a8ad7c4aac
shAdowplusing <1290232854@qq.com>
2023-08-31 22:26:54 +0800

    indexData continue.

--- lib/model/VideoData.dart
+++
@@ -1,8 +0,0 @@
-class VideoData {
-  static Map<String, Map<String, String>> videoData = {
-    "夏日预告企划": {
-      "bilibili": "//player.bilibili.com/player.html?aid=938701785&bvid=BV1gT4y1k7dz&cid=711347686&page=1",
-      "youtube": "https://www.youtube.com/embed/ZI-GnWGzAMo",
-    }
-  };
-}
