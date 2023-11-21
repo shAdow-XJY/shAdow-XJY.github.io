9ed54d23db8e3b86714e8ed1e123909dc74d65ef
shAdowplusing <1290232854@qq.com>
2023-11-21 20:23:18 +0800

    delete something for future structure and content.

--- lib/indexPage/indexWrite/indexWrite.dart
+++ lib/indexPage/indexWrite/indexWrite.dart
@@ -1,10 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:github_blog/global/mdWidget.dart';
-import 'package:github_blog/model/indexData.dart';
-import '../../global/blurGlass.dart';
-import 'package:list_twolevel/list_A.dart';
-import 'package:list_twolevel/list_B.dart';
+
 class IndexWrite extends StatefulWidget {
   const IndexWrite({Key? key}) : super(key: key);
 
@@ -13,129 +9,48 @@ class IndexWrite extends StatefulWidget {
 }
 
 class _IndexWriteState extends State<IndexWrite> {
-  List<Map<String, String>> oneLevelObj = [];
-  List<String> oneLevelTitles = [];
-
-  List<Map<String, String>> twoLevelObj = [];
-  List<String> twoLevelTitles = [];
-
-  bool _oneLevel = true;
-  bool _twoLevel = false;
-  bool _thrLevel = false;
-  String _selectedCategory = '';
-  String _selectedTitle = '';
-
-
-  void oneLevelInit(List<String> titles) {
-    for (var element in titles) {
-      oneLevelObj
-          .add({'title': element, 'image': 'assets/icon/write/$element.png'});
-    }
-    setState(() {
-      oneLevelObj;
-    });
-  }
-
-  void twoLevelInit(String selectCategory) {
-    twoLevelObj.clear();
-    rootBundle
-        .loadString('assets/writeIndex/$selectCategory.txt')
-        .then((value) => {
-              twoLevelTitles = value.split('\n'),
-              twoLevelTitles.removeLast(),
-              for (var element in twoLevelTitles)
-                {
-                  twoLevelObj.add({
-                    'title': element,
-                  })
-                },
-              setState(() {
-                twoLevelObj;
-              })
-            });
-  }
-
-
-  @override
-  void initState() {
-    super.initState();
-    // rootBundle
-    //     .loadString('assets/writeIndex/writeCategory.txt')
-    //     .then((value) => {
-    //           print(value.split('\n')),
-    //           oneLevelTitles = value.split('\n'),
-    //           oneLevelTitles.removeLast(),
-    //           oneLevelInit(oneLevelTitles),
-    //         });
-    oneLevelTitles = IndexData.writeData.keys.toList();
-    oneLevelInit(oneLevelTitles);
-  }
-
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: _oneLevel
-          ? AListBuilder(
-        levelObj: oneLevelObj,
-        onPressed: (selectedCategory) {
-          _oneLevel = false;
-          _twoLevel = true;
-          _selectedCategory = selectedCategory;
-          twoLevelInit(_selectedCategory);
-          setState(() {
-            _oneLevel;
-          });
-        },
-      )
-          : Stack(
-        children: [
-          !_thrLevel
-              ? BListBuilder(
-            levelObj: twoLevelObj,
-            itemHeight: 50,
-            onPressed: (selectedTitle) {
-              _selectedTitle = selectedTitle;
-              _twoLevel = false;
-              _thrLevel = true;
-              setState(() {
-                _thrLevel;
-              });
-            },
-          )
-              : MdWidget(
-            title: _selectedTitle.substring(
-                0, _selectedTitle.lastIndexOf('.')),
-            path: 'assets/write/$_selectedCategory/$_selectedTitle',
-          ),
-          Positioned(
-              bottom: 10.0,
-              right: 10.0,
-              child: BlurGlass(
-                  marginValue: 5.0,
-                  paddingValue: 5.0,
-                  child: IconButton(
-                    icon: const Icon(Icons.arrow_back),
-                    color: Colors.white,
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onPressed: () {
-                      if (_twoLevel) {
-                        setState(() {
-                          _oneLevel = true;
-                        });
-                        _twoLevel = false;
-                      } else if (_thrLevel) {
-                        setState(() {
-                          _thrLevel = false;
-                        });
-                        _twoLevel = true;
-                      }
-                    },
-                  )))
-        ],
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: theme.canvasColor,
+      ),
+      child: Column(
+          children:[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset('assets/image/avatar.jpg'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            AutoSizeText(
+              'ShadowPlus \n 页面清空待重写',
+              minFontSize: 10.0,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w900,
+                  shadows: [
+                    BoxShadow(
+                        blurRadius: 5,
+                        color: Colors.white.withOpacity(0.54)),
+                  ]),
+            ),
+
+          ]
       ),
     );
-
   }
 }
