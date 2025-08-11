2b3205f943167682503a9beb16b8171972a906f1
ShadowPlusing <1290232854@qq.com>
2025-08-11 22:40:30 +0800

    Fix： webview vedio fixed

--- lib/global/videoWidget/webviewWidget.dart
+++ lib/global/videoWidget/webviewWidget.dart
@@ -1,117 +1,119 @@
+import 'dart:ui_web' as ui; // 新导入，替代 platformViewRegistry
+import 'dart:html' as html;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
-import 'package:webviewx/webviewx.dart';
+import '../../innerAssets/videoAsset/videoData.dart';
 
 class WebviewWidget extends StatefulWidget {
-  String title;
-  int source;   // 1: youtobe; 2: bilibili;
-  WebviewWidget({Key? key, required this.title,required this.source}) : super(key: key);
+  final String title;
+  final int source; // 1: youtube; 2: bilibili
+
+  const WebviewWidget({
+    Key? key,
+    required this.title,
+    required this.source,
+  }) : super(key: key);
 
   @override
-  _WebviewWidgetState createState() => _WebviewWidgetState();
+  State<WebviewWidget> createState() => _WebviewWidgetState();
 }
 
 class _WebviewWidgetState extends State<WebviewWidget> {
-  //loading
+  late String viewType;
   bool loading = true;
-  late WebViewXController webviewController;
-
-  String loadPath = "";
-  String partContent = '';
-
-  //mobile\pc inset
-  var padding = const EdgeInsets.symmetric(vertical: 50.0, horizontal: 100.0);
-
-  Future<String> readFile(path){
-    return rootBundle.loadString(path);
-  }
 
   @override
   void initState() {
     super.initState();
-    switch(widget.source){
-      case 1:
-        {
-          loadPath = 'assets/videoIndex/${widget.title}.youtube';
-          readFile(loadPath).then((value) => {
-            setState((){
-              partContent = ' src="$value" scrolling="no" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen';
-            })
-          });
-        }
-        break;
-      case 2:
-        {
-          loadPath = 'assets/videoIndex/${widget.title}.bilibili';
-          readFile(loadPath).then((value) => {
-            setState((){
-              partContent = ' src="$value&high_quality=1&danmaku=0" allowfullscreen="allowfullscreen" scrolling="no" frameborder="0" sandbox="allow-top-navigation allow-same-origin allow-forms allow-scripts"';
-            })
-          });
-        }
-        break;
-      default: break;
+
+    final videoUrls = VideoData.videoData[widget.title];
+    String iframeHtml = '';
+    if (videoUrls != null) {
+      iframeHtml = widget.source == 1
+          ? (videoUrls['youtube'] ?? '')
+          : (videoUrls['bilibili'] ?? '');
     }
 
+    if (iframeHtml.isEmpty) {
+      iframeHtml = '<p style="color:white;">视频未找到</p>';
+    }
 
-  }
+    // 生成唯一 viewType
+    viewType = 'iframe_${DateTime.now().millisecondsSinceEpoch}';
 
-  @override
-  void dispose() {
-    webviewController.dispose();
-    super.dispose();
+    // 注册 HTML iframe
+    ui.platformViewRegistry.registerViewFactory(
+      viewType,
+          (int viewId) {
+        final element = html.DivElement()
+          ..setInnerHtml(
+            iframeHtml,
+            validator: html.NodeValidatorBuilder.common()
+              ..allowElement('iframe', attributes: [
+                'src',
+                'style',
+                'frameborder',
+                'allow',
+                'allowfullscreen',
+                'scrolling',
+                'referrerpolicy'
+              ])
+              ..allowHtml5(),
+          )
+          ..style.width = '100%'
+          ..style.height = '100%';
+
+        Future.delayed(const Duration(milliseconds: 300), () {
+          if (mounted) {
+            setState(() => loading = false);
+          }
+        });
+
+        return element;
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return ResponsiveBuilder(
-        builder: (context, sizingInformation) {
-          if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
-            padding = const EdgeInsets.symmetric(vertical: 100.0, horizontal: 5.0);
-          }
-          return Dialog(
-            backgroundColor: Colors.black,
-            // 背景色
-            insetPadding: padding,
-            // 弹框距离屏幕边缘距离
-            child: Container(
-                color: Colors.transparent,
-                height: size.height,
-                width: size.width,
-                child:
-                Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      WebViewX(
-                        height: size.height,
-                        width: size.width,
-                        initialSourceType: SourceType.html,
-                        initialContent: 'about:blank',
-                        onWebViewCreated: (controller) => {
-                          webviewController = controller,
-                          webviewController.loadContent('<iframe width="100%" height="${size.height/1.3}" $partContent ></iframe>', SourceType.html,),
-                        },
-                        onPageFinished: (value){
-                          setState((){loading = false;});
-                        },
-                      ),
-                      loading
-                          ?const Center(
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.purple,
-                        ),
-                      )
-                          :const SizedBox(),
-                    ]
-                )
 
-            ),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        var padding = const EdgeInsets.symmetric(
+          vertical: 50.0,
+          horizontal: 100.0,
+        );
+        if (sizingInformation.deviceScreenType == DeviceScreenType.mobile) {
+          padding = const EdgeInsets.symmetric(
+            vertical: 100.0,
+            horizontal: 5.0,
           );
         }
-    );
 
+        return Dialog(
+          backgroundColor: Colors.black,
+          insetPadding: padding,
+          child: Stack(
+            children: [
+              // 居中 + 16:9 宽高比
+              Center(
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: HtmlElementView(viewType: viewType),
+                ),
+              ),
+              if (loading)
+                const Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.purple,
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/innerAssets/videoAsset/videoData.dart
+++ lib/innerAssets/videoAsset/videoData.dart
@@ -1,8 +1,25 @@
 class VideoData {
   static Map<String, Map<String, String>> videoData = {
     "夏日预告企划": {
-      "bilibili": "//player.bilibili.com/player.html?aid=938701785&bvid=BV1gT4y1k7dz&cid=711347686&page=1",
-      "youtube": "https://www.youtube.com/embed/ZI-GnWGzAMo",
+      "bilibili": '''
+      <iframe 
+        src="//player.bilibili.com/player.html?isOutside=true&aid=938701785&bvid=BV1gT4y1k7dz&cid=711347686&p=1" 
+        style="width:100%; aspect-ratio:16/9; border:none;" 
+        frameborder="0" 
+        scrolling="no" 
+        allowfullscreen="true">
+      </iframe>
+      ''',
+      "youtube": '''
+      <iframe 
+        src="https://www.youtube.com/embed/ZI-GnWGzAMo?si=NeJcJzjkGjgHXJiN" 
+        style="width:100%; aspect-ratio:16/9; border:none;" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+      </iframe>
+      ''',
     }
   };
 }
